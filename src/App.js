e474e9b178962e44a35fdc4d02fbdb804766013c
Fateme Fazelzade <fatemefazelzade30@gmail.com>
2022-06-27 22:55:43 +0430

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+
+
+function App() {
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=paris&appid=31a61759a72f4a12a6a2bbc204dd8c52" 
+
+  return (
+    <div className="app">
+
+    </div>
+  );
+}
+
+export default App;
