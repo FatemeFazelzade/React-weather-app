80dfb9c7a760668b165686685a0a4bf5b0fecb17
Fateme Fazelzade <83185714+FatemeFazelzade@users.noreply.github.com>
2022-06-28 17:09:01 +0430

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,7 @@ import '../Styles/Style.css';
 
 
 const api = {
-  key: "31a61759a72f4a12a6a2bbc204dd8c52",
+  key: "",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 // const url = "https://api.openweathermap.org/data/2.5/weather?q=paris&appid=31a61759a72f4a12a6a2bbc204dd8c52" 
