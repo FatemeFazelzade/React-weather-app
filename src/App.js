f10b5535f386fcb660266de67c7854f80e8b086a
FatemeFazelzade <fatemefazelzde30@gmail.com>
2022-06-28 17:02:45 +0430

    Add location and temp features

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from 'react';
 
-
+import Weather from './components/Weather';
 
 function App() {
-
-const url = "https://api.openweathermap.org/data/2.5/weather?q=paris" 
-
   return (
-    <div className="app">
-
+    <div className="App">
+      <Weather />
     </div>
   );
 }
