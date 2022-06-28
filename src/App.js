1c58e5eb7a540b85b7bbd6ae31bab15eb6c4f410
Fateme Fazelzade <83185714+FatemeFazelzade@users.noreply.github.com>
2022-06-28 12:07:53 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 function App() {
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=paris&appid=31a61759a72f4a12a6a2bbc204dd8c52" 
+const url = "https://api.openweathermap.org/data/2.5/weather?q=paris" 
 
   return (
     <div className="app">
